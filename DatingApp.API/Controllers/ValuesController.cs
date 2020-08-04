c4fc1110802599a58a56cd4298660d3bbee34df0
aucanpool <angelucanpool@gmail.com>
2020-08-04 13:31:16 -0500

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        public DataContext _context { get; }
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        
+        [HttpGet]
+        public async Task<IActionResult> GetValues(){
+           var values=await _context.Values.ToListAsync();
+            return Ok(values);
+            //return Ok("Puto");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id){
+            var value=await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(value);
+        }
+
+        
+    }
+}
